b22b2f074f949c120dc5ff57acd99e06e7104ab9
mateib47 <m.bucur@student.utwente.nl>
2021-09-12 21:17:16 +0200

    added why choose us section and improved the menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ function showModal(modal) {
 function hideModal(modal) {
   modal.style.display = 'none';
 }
+window.onclick = function(event){
+  let modals = document.getElementsByClassName("modal");
+  for(let i=0; i < modals.length; i++){
+    if(event.target == modals[i]){
+      hideModal(modals[i]);
+    }
+  }
+}
