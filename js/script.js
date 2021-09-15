a4dc3567683a3b738a5a5820c1738a777f27b15b
mateib47 <m.bucur@student.utwente.nl>
2021-09-15 21:33:09 +0200

    added email input and user clicks are stored in local memory for now

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let choices = [];//getters and setters
+
 function showModal(modal) {
   modal.style.display = 'block';
 }
@@ -13,3 +15,52 @@ window.onclick = function(event){
     }
   }
 }
+
+let boxes = document.querySelector("#plans").querySelectorAll(".plan-box");
+for(let x of boxes){
+  x.onclick = function() { addChoice(x.getAttribute('value'));
+  };
+}
+
+function addChoice(text) {
+  const choice = {
+    id : Date.now(),
+    name: text,
+    counter:0
+  }
+  let prev = choices.find(elem => elem.name == text);
+  if (prev){
+    prev.counter++;
+  }else{
+    choices.push(choice);
+  }
+  localStorage.setItem('choiceRef', JSON.stringify(choices));
+}
+
+const form = document.querySelector("#email-form");
+form.addEventListener('submit',event => emailForm('#email-input'));
+
+function emailForm(inputId){
+  event.preventDefault();
+  const input = document.querySelector(inputId);
+  const text = input.value.trim();
+  if(text !== ''){
+    addEmail(text);
+    input.value = '';
+  }
+}
+
+function addEmail(text){
+  const user = {
+    id : Date.now(),
+    email:text
+  }
+  localStorage.setItem('emailRef', JSON.stringify(user));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const choiceRef = localStorage.getItem('choiceRef');
+  if (choiceRef){
+    choices = JSON.parse(choiceRef);
+  }
+});
