fe428a3a1f6227ca8443c479f73be345c2599206
mateib47 <m.bucur@student.utwente.nl>
2021-09-24 23:51:09 +0200

    added contact section and modified some content

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ function addEmail(text){
   }
   localStorage.setItem('emailRef', JSON.stringify(user));
   let xhr = new XMLHttpRequest();
-  const url = "http://192.168.2.13:8080/api/v1/person";
+  const url = "http://localhost:8080/api/v1/person";
   xhr.open("POST",url , true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({
